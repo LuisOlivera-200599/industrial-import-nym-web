ab88fc2e38ed4fb13aca6435d20e9c4cda68f3fa
LuisOlivera-200599 <oliveravelasquezluis@gmail.com>
2026-04-24 13:38:13 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://lzaolbleriqwbmnejypy.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_LiAAKOOvCkfc7tWuIdzERg_UsOUpltv";
 
-const nymSupabase = supabase.createClient(
+window.nymSupabase = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
